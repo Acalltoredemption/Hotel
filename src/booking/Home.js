433c512544ca3621f8482bff0cec9d455efd1b66
Acalltoredemption <acalltoredemption@gmail.com>
2021-03-12 10:28:43 -0800

    message

--- src/booking/Home.js
+++ src/booking/Home.js
@@ -1,8 +1,12 @@
+import {useSelector} from 'react-redux';
+
+
 const Home = () => {
+    const {user} = useSelector((state) => ({...state}));
     return (
         <div className="container-fluid h1 p-5 text-center">
-            Home Page
-        </div>
+            Home Page {JSON.stringify(user)}
+        </div> 
     );
 };
 
