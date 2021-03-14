475c855cf0e3df2025129f14f4fd830d0a0f3434
Acalltoredemption <acalltoredemption@gmail.com>
2021-03-14 10:37:39 -0700

    adding loginform component and some validation to login and register

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,7 @@
-const RegisterForm = ({handleSubmit, name, setName, email, setEmail, password, setPassword}) => {
+const LoginForm = ({handleSubmit, email, setEmail, password, setPassword}) => {
     return (
         <form onSubmit={handleSubmit} className="mt-3">
-            <div className="form-group mb-3">
-                <label className="form-label mb-3">Your Name</label>
-                <input type="text" className="form-control" placeholder="Enter Name" value={name} onChange={e => setName(e.target.value)} />
-            </div>
+
             <div className="form-group mb-3">
                 <label className="form-label mb-3">Email</label>
                 <input type="email" className="form-control" placeholder="Enter Email" value={email} onChange={e => setEmail(e.target.value)} />
@@ -13,9 +10,9 @@ const RegisterForm = ({handleSubmit, name, setName, email, setEmail, password, s
                 <label className="form-label mb-3">Password</label>
                 <input type="password" className="form-control" placeholder="Enter Password" value={password} onChange={e => setPassword(e.target.value)} />
             </div>
-            <button disabled={!name || !email || !password} className="btn btn-primary">Submit</button>
+            <button disabled={!email || !password} className="btn btn-primary">Submit</button>
         </form>
     )
 };
 
-export default RegisterForm;
+export default LoginForm;
