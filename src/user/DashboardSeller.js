5f4d8d932b2e9e1a303fd30e18d4aec1d6cce37c
Acalltoredemption <acalltoredemption@gmail.com>
2021-03-16 18:30:43 -0700

    creating stripe onboarding for users who do not have stripe connected yet

--- src/user/DashboardSeller.js
+++ src/user/DashboardSeller.js
@@ -1,18 +1,14 @@
 import DashboardNav from '../components/DashboardNav';
 import ConnectNav from '../components/ConnectNav';
 import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux';
+import {HomeOutlined} from '@ant-design/icons';
+
 
 const DashboardSeller = () => {
-    return(
-        <>
-        <div className="container-fluid bg-secondary p-5">
-            <ConnectNav />
-        </div>
+    const {auth} = useSelector((state) => ({...state}));
 
-        <div className="container-fluid p-4">
-            <DashboardNav />
-        </div>
-
+    const connected = () => (
         <div className="container-fluid">
            <div className="row">
                <div className="col-md-10">
@@ -23,6 +19,38 @@ const DashboardSeller = () => {
                </div>
            </div>
         </div>
+    );
+
+    const notConnected = () => (
+        <div className="container-fluid">
+        <div className="row">
+            <div className="col-md-6 offset-md-3 text-center">
+                <div className="p-5 pointer">
+                    <HomeOutlined className="h2" />
+                <h4>Setup payouts to post hotel rooms</h4>
+                <p className="lead">MERN partners with Stripe to transfer earnings to your bank account</p>
+                <button className="btn btn-primary mb-3">Setup Payouts</button>
+                <p className="text-muted"><small>You'll be redirected to Stripe to complete the onboarding process</small></p>
+                </div>
+            </div>
+        </div>
+     </div>
+    );
+
+
+    return(
+        <>
+        <div className="container-fluid bg-secondary p-5">
+            <ConnectNav />
+        </div>
+
+        <div className="container-fluid p-4">
+            <DashboardNav />
+        </div>
+
+        {auth && auth.user && auth.user.stripe_seller && auth.user.stripe_seller.charges_enabled ? connected() : notConnected()}
+
+        
         </>
     )
 };
