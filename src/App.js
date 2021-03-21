82f02b5cca09a0f5091b5f8bbf91b2231a0ee7f1
Acalltoredemption <acalltoredemption@gmail.com>
2021-03-21 15:49:34 -0700

    preparing app frontend to get user stripe status and account status for stripecallback

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Dashboard from './user/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
 import DashboardSeller from './user/DashboardSeller';
 import NewHotel from './hotels/NewHotel';
+import StripeCallback from './stripe/StripeCallback';
 
 //
 
@@ -26,6 +27,7 @@ function App() {
       <PrivateRoute exact path="/dashboard" component={Dashboard} />
       <PrivateRoute exact path="/dashboard/seller" component={DashboardSeller} />
       <PrivateRoute exact path="/hotels/new" component={NewHotel} />
+      <PrivateRoute exact path="/stripe/callback" component={StripeCallback} />
     </Switch>
     </BrowserRouter>
   ); 
