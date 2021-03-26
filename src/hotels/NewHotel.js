6630a3f137d06e25caa54247cd6fd118b5c9f264
Acalltoredemption <acalltoredemption@gmail.com>
2021-03-25 18:26:51 -0700

    updating newhotel component and preparing handlesubmit to send our form data

--- src/hotels/NewHotel.js
+++ src/hotels/NewHotel.js
@@ -4,6 +4,7 @@ import {DatePicker, Select} from 'antd';
 import moment from 'moment';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import {createHotel} from '../actions/hotel';
+import {useSelector} from 'react-redux';
 
 const {Option} = Select;
 
@@ -18,6 +19,9 @@ const config = {
 
 
 const NewHotel = () => {
+    //redux
+    const {auth} = useSelector((state) => ({...state}));
+    const {token} = auth;
     //state
     const [values, setValues] = useState({
         title: '',
@@ -33,10 +37,27 @@ const NewHotel = () => {
     const [location, setLocation] = useState('')
     //destructuring variables from state
     const {title, content, image, price, from, to, bed} = values;
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(values);
-        console.log(location.label);
+        let hotelData = new FormData();
+        hotelData.append('title', title);
+        hotelData.append('content', content);
+        hotelData.append('location', JSON.stringify(location.label));
+        hotelData.append('price', price);
+        image && hotelData.append('image', image);
+        hotelData.append('from', from);
+        hotelData.append('to', to);
+        hotelData.append('bed', bed);
+
+        console.log([...hotelData]);
+
+        let res = await createHotel(token, hotelData);
+        console.log('HOTEL CREATE RES', res)
+        toast('New hotel is posted')
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000)
+
     }
 
     const handleImageChange = (e) => {
