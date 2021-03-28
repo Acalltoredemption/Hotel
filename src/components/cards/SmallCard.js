4294a41986cfa936a9e4594051e5fbb954f033de
Acalltoredemption <acalltoredemption@gmail.com>
2021-03-28 16:13:30 -0700

    displaying hotel image

--- src/components/cards/SmallCard.js
+++ src/components/cards/SmallCard.js
@@ -12,7 +12,11 @@ return (
     <div className="card mb-3">
         <div className="row no-gutters">
             <div className="col-md-4">
-                <img src="https://via.placeholder.com/900x500.png?text=MERN+Booking" alt="default hotel image" className="card-image img img-fluid" />
+                {h.image && h.image.contentType ? (
+                    <img src={`${process.env.REACT_APP_API}/hotel/image/${h._id}`} alt="default hotel image" className="card-image img img-fluid" />
+                ) : (
+                    <img src="https://via.placeholder.com/900x500.png?text=MERN+Booking" alt="default hotel image" className="card-image img img-fluid" />
+                )}
             </div>
             <div className="col-md-8">
                 <div className="card-body">
