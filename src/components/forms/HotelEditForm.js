0e7ab79c606ce05173004e8ac7364c86741b1696
Acalltoredemption <acalltoredemption@gmail.com>
2021-03-30 10:42:48 -0700

    edithotel and hoteleditform changes

---
+++ src/components/forms/HotelEditForm.js
@@ -0,0 +1,70 @@
+import {DatePicker, Select} from 'antd';
+import moment from 'moment';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
+
+const {Option} = Select;
+
+
+const config = {
+    appId: process.env.REACT_APP_ALGOLIA_APP_ID,
+    apiKey: process.env.REACT_APP_ALGOLIA_API_KEY,
+    language: 'en',
+    // countries: ['au'],
+};
+
+
+
+const HotelEditForm = (props) => {
+    const {values, setValues, handleChange, handleImageChange, handleSubmit, location, setLocation} = props;
+    const {title, content, price, bed, from, to} = values;
+
+    return ( 
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label className="btn btn-outline-secondary btn-block m-2 text-left">
+                    Image
+                    <input type="file" name="image" onChange={handleImageChange} accept="image/*" hidden />
+                </label>
+                <input type="text" name="title" onChange={handleChange} placeholder="Title" className="form-control m-2" value={title} />
+                <textarea name="content" onChange={handleChange} placeholder="Content" className="form-control m-2" value={content} />
+
+
+             <GooglePlacesAutocomplete  className="form-control ml-2 mr-2" name="locs" placeholder="Location"  selectProps={{
+          location,
+          onChange: setLocation,
+        }}defaultValue={location} style={{height: "50px"}} />
+
+
+                <input type="number" name="price" onChange={handleChange} placeholder="Price" className="form-control m-2" value={price} />
+
+
+                <Select onChange={(value) => setValues({...values, bed: value})} className="w-100 m-2" size="large" placeholder="Number of Beds"
+                value={bed}>
+                    <Option key={1}>{1}</Option>
+                    <Option key={2}>{2}</Option>
+                    <Option key={3}>{3}</Option>
+                    <Option key={4}>{4}</Option>
+                </Select>
+
+
+            </div>
+
+
+            {from && (
+            <DatePicker defaultValue={moment(from, "YYYY-MM-DD")} placeholder="From date" className="form-control m-2" disabledDate={(current) => current && current.valueOf() < moment().subtract(1, 'days')} onChange={(date, dateString) => setValues({...values, from:dateString})} /> )}
+
+
+            {to && (<DatePicker defaultValue={moment(to, "YYYY-MM-DD")} placeholder="To date" className="form-control m-2" disabledDate={(current) => current && current.valueOf() < moment().subtract(1, 'days')} onChange={(date, dateString) => setValues({...values, to:dateString})} />)}
+
+
+            <button className="btn btn-outline-primary m-2">Save</button>
+
+        </form>
+    );
+};
+
+export default HotelEditForm;
+
+
+
--- src/hotels/EditHotel.js
+++ src/hotels/EditHotel.js
@@ -1,8 +1,10 @@
 import {useState, useEffect} from 'react';
 import {toast} from 'react-toastify';
 import {DatePicker, Select} from 'antd';
-import {read} from '../actions/hotel';
+import {read, updateHotel} from '../actions/hotel';
 import {useSelector} from 'react-redux';
+import HotelEditForm from '../components/forms/HotelEditForm';
+
 
 const {Option} = Select;
 
@@ -15,15 +17,17 @@ const EditHotel = ({match}) => {
     const [values, setValues] = useState({
         title: '',
         content: '',
-        image: '',
         price: '',
         from: '',
         to: '',
         bed: '',
     });
-    const [preview, setPreview] = useState('https://via.placeholder.com/100x100.png?text=PREVIEW');
 
-    const {title, content, image, price, from, to, bed} = values;
+    const [image, setImage] = useState('');
+    const [preview, setPreview] = useState('https://via.placeholder.com/100x100.png?text=PREVIEW');
+    const [location, setLocation] = useState('')
+
+    const {title, content, price, from, to, bed} = values;
 
 
     useEffect(() => {
@@ -34,18 +38,39 @@ const EditHotel = ({match}) => {
         let res = await read(match.params.hotelId)
         // console.log(res);
         setValues({...values, ...res.data});
-        setPreview(`${process.env.REACT_APP_API}/hotel/image/res.data._id`);
+        setPreview(`${process.env.REACT_APP_API}/hotel/image/${res.data._id}`);
     };
 
     const handleSubmit = async (e) => {
+        e.preventDefault();
 
+        let hotelData = new FormData();
+        hotelData.append('title', title);
+        hotelData.append('content', content);
+        hotelData.append('location', JSON.stringify(location.label));
+        hotelData.append('price', price);
+        image && hotelData.append('image', image);
+        hotelData.append('from', from);
+        hotelData.append('to', to);
+        hotelData.append('bed', bed);
+
+
+        try {
+            let res = await updateHotel(token, hotelData, match.params.hotelId)
+            console.log('HOTEL UPDATE RES', res);
+            toast.success(`${res.data.title} is updated`);
+        
+        } catch(err){
+            console.log(err);
+            toast.error(err.response.data.err);
+        }
     };
 
     
     const handleImageChange = (e) => {
         // console.log(e.target.files[0]);  
         setPreview(URL.createObjectURL(e.target.files[0]));  
-        setValues({...values, image: e.target.files[0]});
+        setImage(e.target.files[0]);
          }
      
     const handleChange = (e) => {
@@ -61,7 +86,16 @@ const EditHotel = ({match}) => {
             <div className="row">
                 <div className="col-md-10">
                     <br/>
-                    show edit form
+                    <HotelEditForm 
+                    values={values}
+                    setValues={setValues}
+                    handleChange={handleChange}
+                    handleImageChange={handleImageChange}
+                    handleSubmit={handleSubmit}
+                    location={location}
+                    setLocation={setLocation}
+                    
+                    />
                 </div>
                 <div className="col-md-2">
                 <img src={preview} alt="Preview_image" className="img img-fluid m-2"/>
